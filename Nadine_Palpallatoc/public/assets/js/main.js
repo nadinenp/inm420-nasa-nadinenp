e6935d137c2045f220fc33086cf5700d6200ea13
nadinenp <napalpallatoc@myseneca.ca>
2024-02-15 10:51:37 -0500

    2

--- Nadine_Palpallatoc/public/assets/js/main.js
+++ Nadine_Palpallatoc/public/assets/js/main.js
@@ -1,38 +1,41 @@
 console.log('try');
 
-// this is to define a function to fetch data from the NASA APOD API
-async function fetchAPOD() {
-    try {
-        // this is to get the user-selected date from the input field
-        const date = document.getElementById('dateInput').value;
+// This is the "Get Photo" button
+const getPhotoButton = document.getElementById('getPhotoButton');
+getPhotoButton.addEventListener('click', () => {
+    const date = document.getElementById('dateInput').value;
+    fetchAPOD(date);
+});
 
-        // my NASA API key
+// This is to define the function to fetch data from the NASA APOD API
+async function fetchAPOD(date) {
+    try {
+        // My NASA API key
         const apiKey = 'S1gh39gdmF1e2G0HP0rMqEnad4NtkSWI0xJcLR0f';
 
         // API URL with the date + API key
         const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`;
 
-        // this is to get request to the API
+        // Send request to the API
         const response = await fetch(apiUrl);
 
-        // parse the response as JSON
+        // Parse the response as JSON
         const data = await response.json();
 
-        // this is to extract the image URL and explanation from the response data
+        // This is to extract the image URL and explanation from the response data
         const imageUrl = data.url;
         const explanation = data.explanation;
 
-        // to display the image and explanation
+        // This is to display the image and explanation
         const apodImage = document.getElementById('apodImage');
-        apodImage.src = imageUrl; // image source
-        apodImage.alt = 'Astronomy Picture of the Day'; // Set the image alt text
+        apodImage.src = imageUrl; // Image source
+        apodImage.alt = 'Astronomy Picture of the Day'; 
 
         const apodExplanation = document.getElementById('apodExplanation');
-        apodExplanation.textContent = explanation; // explanation text
-    } 
-    
-    catch (error) {
-        // errors
+        apodExplanation.textContent = explanation; // Explanation text
+    } catch (error) {
+
+        // Handle errors
         console.error('Error fetching APOD:', error);
     }
 }
